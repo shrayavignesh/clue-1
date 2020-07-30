50e3c6757f772a3ac19eda32c4945fb871b9a73e
LP-Diggle <68982433+LP-Diggle@users.noreply.github.com>
2020-07-31 11:00:45 +1200

    Player.java
    Added getters and setters, addCard() and changed ArrayList<Card> to List<Card>

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Player {
-    final ClueCharacter clueCharacter;
-    private ArrayList<Card> hand = new ArrayList<>();
+	
+	final ClueCharacter clueCharacter;
+    private List<Card> hand = new ArrayList<>();
     private Room currentRoom = null;
     private Room previousRoom = null;
     private boolean canPlay = true;
@@ -10,4 +13,24 @@ public class Player {
     public Player(ClueCharacter character) {
         clueCharacter = character;
     }
+    
+    /**
+     * Adds card to hand
+     * @param card to be added
+     */
+    public void addCard(Card card) {
+    	hand.add(card);
+    }
+    
+    // Getters
+    public List<Card> getHand(){return Collections.unmodifiableList(hand);}
+    public Room getCurrentRoom(){return currentRoom;}
+    public Room getPreviousRoom(){return previousRoom;}
+    public boolean canStillPlay(){return canPlay;}
+    
+    // Setters
+    public void setCurrentRoom(Room r){currentRoom = r;}
+    public void setPreviousRoom(Room r){previousRoom = r;}
+    public void setPlayStatus(boolean b){canPlay = b;}
+	
 }
