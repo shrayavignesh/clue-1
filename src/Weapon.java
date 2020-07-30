ce08d5919e7c58b98c4b8f4cca028cf017da2d9c
LP-Diggle <68982433+LP-Diggle@users.noreply.github.com>
2020-07-31 11:09:55 +1200

    Weapon.java
    Added constructor and cardDescription()

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,2 +1,13 @@
 public class Weapon extends Card {
+  
+    private String weaponName;
+  
+    public Weapon(String weaponName){
+      this.weaponName = weaponName;
+    }
+  
+    @Override
+    public String cardDescription(){
+      return "This weapon is a "+weaponName+".\n";
+    }
 }
