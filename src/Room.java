1a54d457642f685bcfb9041a77afbb2905f75e86
LP-Diggle <68982433+LP-Diggle@users.noreply.github.com>
2020-07-31 11:28:00 +1200

    Room.java
    Added otherWeapon field, constructor, addWeapon(), addCharacter(), bringCharacterToRoom(), bringWeaponToRoom() and cardDescription()

--- src/Room.java
+++ src/Room.java
@@ -2,5 +2,48 @@ public class Room extends Card {
     ClueCharacter character = null;
     ClueCharacter other = null;
     Weapon weapon;
+    Weapon otherWeapon;
     String roomName;
+    
+    public Room(String roomName){
+        this.roomName = roomName;
+    }
+    
+    /**
+    * Adds weapon to room when game is made, may not have a weapon
+    */
+    public void addWeapon(Weapon weapon){
+        this.weapon = weapon;
+    }
+    
+    /**
+    * Adds character to the room
+    */
+    public void addCharacter(ClueCharacter character){
+        this.character = character;
+    }
+    
+    /**
+    * Only used if a character has been named in a suggestion and they are not in this room
+    */
+    public void bringCharacterToRoom(ClueCharacter other){
+        this.other = other;
+    }
+    
+    /**
+    * Only used if a weapon has been named in a suggestion and they are not in this room
+    */
+    public void bringWeaponToRoom(Weapon otherWeapon){
+        this.otherWeapon = otherWeapon;
+    }
+    
+    @Override
+    public String cardDescription(){
+        if(weapon == null){
+            return "You've entered the "+roomName+" room, there's nothing in here.\n";
+        }
+        else{
+            return "You've entered the "+roomName+" room, you see a "+weapon.cardDescription()+".\n";
+        }
+    }
 }
