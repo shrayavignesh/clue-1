b4afd923f641deaacd8e4f99991bcadfe74355c1
Blair Burton <blair.burton@outlook.com>
2020-08-04 09:18:16 +1200

    Add files via upload (#2)
    Co-authored-by: laurencemalata <50721936+laurencemalata@users.noreply.github.com>

--- src/Room.java
+++ src/Room.java
@@ -1,5 +1,11 @@
+package Cluedo;
+
 public class Room extends Card {
     ClueCharacter character = null;
     ClueCharacter other = null;
     Weapon weapon;
+
+    public Room (String name){
+        this.name = name;
+    }
 }
--- src/Suggestion.java
+++ src/Suggestion.java
@@ -1,11 +1,66 @@
+package Cluedo;
+
+import Cluedo.Card;
+import Cluedo.ClueCharacter;
+import Cluedo.Player;
+import Cluedo.Room;
+
+import java.util.ArrayList;
+
 public class Suggestion {
+    //Curent Cluedo.Player
     final Weapon weapon;
     final ClueCharacter character;
     final Room room;
+    final Player player;
 
-    public Suggestion(Weapon weapon, ClueCharacter character, Room room) {
+    //Neighbouring Cluedo.Player
+    private Player player_with_Card;
+    private ArrayList<Card> presentCards;
+
+    public Suggestion(Weapon weapon, ClueCharacter character, Room room, Player player) {
         this.weapon = weapon;
         this.character = character;
         this.room = room;
+        this.player = player;
+    }
+
+    /*
+        A boolean method that check if the neighbouring player has the suggested cards. If true, iteration
+        going to the next players stop.
+
+        During this method, if the player has a suggested card, the specific card will be added to the presentCards
+        ArrayList.. this is to assume that the player has either 1 to 3 suggested cards in their hand
+
+        Author: Laurence Malata
+     */
+    public boolean checkHand (ArrayList<Card> hand, Player nextPlayer){
+        boolean present = false;
+        for(Card card : hand){
+            if(card.equals(character) || card.equals(room) || card.equals(weapon)){
+                present = true;
+                player_with_Card = nextPlayer;
+                presentCards.add(card);
+            }
+        }
+        return present;
+    }
+
+    /*
+        Returns a neighbouring player with a suggested card
+
+        Author: Laurence Malata
+     */
+    public Player getPlayerWithCard(){
+        return player_with_Card;
+    }
+
+    /*
+        Returns the present suggested cards of the neighbouring player
+
+        Author: Laurence Malata
+     */
+    public ArrayList<Card> getPresentCards(){
+        return presentCards;
     }
 }
