3e907238daafb8f96415995bd52d6e3b99cba754
Blair Burton <blair.burton@outlook.com>
2020-08-04 13:18:20 +1200

    Removed unused imports

--- src/Clue.java
+++ src/Clue.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Queue;
 
 import javafx.util.Pair;
