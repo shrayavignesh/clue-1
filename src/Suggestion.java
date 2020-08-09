de1ceedf1d09436eb3fce82a9b2eeb6186011485
laurencemalata <50721936+laurencemalata@users.noreply.github.com>
2020-08-09 20:27:56 +1200

    Update Suggestion.java

--- src/Suggestion.java
+++ src/Suggestion.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Suggestion {
     // Current Player
@@ -8,7 +9,7 @@ public class Suggestion {
 
     //Neighbouring Player
     private Player playerWithCard;
-    private ArrayList<Card> presentCards;
+    private List<Card> presentCards;
 
     public Suggestion(Weapon weapon, ClueCharacter character, Room room) {
         this.weapon = weapon;
@@ -17,16 +18,16 @@ public class Suggestion {
 
     }
 
-    /*
+    /**
         A boolean method that check if the neighbouring player has the suggested cards. If true, iteration
         going to the next players stop.
 
         During this method, if the player has a suggested card, the specific card will be added to the presentCards
         ArrayList.. this is to assume that the player has either 1 to 3 suggested cards in their hand
 
-        Author: Laurence Malata
+        @Author:Laurence_Malata
      */
-    public boolean checkHand(ArrayList<Card> hand, Player nextPlayer) {
+    public boolean checkHand(List<Card> hand, Player nextPlayer) {
         boolean present = false;
         for (Card card : hand) {
             if (card.equals(character) || card.equals(room) || card.equals(weapon)) {
@@ -38,24 +39,26 @@ public class Suggestion {
         return present;
     }
 
-    /*
+    /**
         Returns a neighbouring player with a suggested card
-
-        Author: Laurence Malata
+        @Author:Laurence_Malata
      */
     public Player getPlayerWithCard() {
         return playerWithCard;
     }
 
-    /*
-        Returns the present suggested cards of the neighbouring player
-
-        Author: Laurence Malata
+    /**
+        Prints suggested cards of the neighbouring player
+       @Author:Laurence_Malata
      */
-    public ArrayList<Card> getPresentCards() {
-        return presentCards;
+    public String printCards(){
+        String cards = "Pick a card to refute: Type the number: \n";
+        int count = 0;
+        for(Card c : presentCards){ cards += count++ + " [" + c.name + "]   "; }
+        return cards;
     }
 
+    public List<Card> getPresentCards() { return presentCards; }
     public ClueCharacter getCharacter() {return character;}
     public Room getRoom() {return room;}
     public Weapon getWeapon() {return weapon;}
